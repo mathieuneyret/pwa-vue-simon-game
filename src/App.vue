081e8604b801d31c0e43a5fc019a410a42234a8c
mathieuneyret <neyret.mathieu69@gmail.com>
2022-11-24 15:03:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import SimonGame from './components/SimonGame.vue'
+</script>
+
+<template>
+  <SimonGame />
+</template>
+
+<style scoped>
+
+</style>
